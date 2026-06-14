5ff339691e4a18b823d3fe9230bb4990ccd8d0c8
MateenSohail-coder <abdulmateensohailking@gmail.com>
2026-06-14 07:59:57 +0500

    fix bug

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -10,15 +10,12 @@ export default function DashboardLayout({ children }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset className="h-screen overflow-hidden flex flex-col bg-background dark:bg-black">
-        <header className="flex fixed top-0 h-14 shrink-0 items-center gap-2 border-b border-border px-4 dark:border-neutral-900 justify-between">
+      <SidebarInset className="h-screen relative overflow-hidden flex flex-col bg-background dark:bg-black">
+        <header className="flex shrink-0 items-center gap-2 border-b border-border px-4 dark:border-neutral-900 justify-between h-14">
           <SidebarTrigger />
         </header>
 
-        {/* Removed redundant p-6 and locked height to match the internal flex structure */}
-        <main className="flex-1 h-[calc(100vh-3.5rem)] overflow-hidden relative">
-          {children}
-        </main>
+        <main className="flex-1 overflow-hidden relative">{children}</main>
       </SidebarInset>
     </SidebarProvider>
   );
